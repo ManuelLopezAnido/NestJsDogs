1bdb51f2f4a25afea2dac2d78623fbb8aa60a9b2
Manuel Lopez Anido <manuellopezanido@gmail.com>
2023-05-30 21:13:31 -0300

    second commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { CatsController } from './cats.controller';
 import { MongooseModule } from '@nestjs/mongoose'
 import { DogsModule } from './dogs/dogs.module';
-
+require('dotenv').config();
+const uri = process.env.MONGODB_URI;
 
 @Module({
   imports: [DogsModule, MongooseModule.forRoot(
-    'mongodb+srv://Manuel:rEciPOJiyudgdaBP@manuprojects.ihycnog.mongodb.net/?retryWrites=true&w=majority', 
+    `mongodb+srv://Manuel:${uri}`, 
   {
     dbName: 'dataDogs',
     connectionName: 'DogDB'
